426e8759540c244526eef4b09293fff5fa70a69a
rtorresuic <rtorresuic@gmail.com>
2013-10-22 15:02:21 -0500

    Update to movement class

--- Bomberman/movement.cpp
+++ Bomberman/movement.cpp
@@ -3,3 +3,25 @@
 movement::movement()
 {
 }
+
+movement::moveUp(int x, int y, int key)
+{
+    y++;
+}
+
+movement::moveDown(int x, int y, int key)
+{
+    y--;
+}
+
+movement::moveLeft(int x, int y, int key)
+{
+    x--;
+}
+
+movement::moveRight(int x, int y, int key)
+{
+    x++;
+}
+
+
