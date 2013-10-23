f04be522f1b40de0ed6d6bb054b793a320bc3cdb
Piotr Gorski <slmblck@gmail.com>
2013-10-23 18:08:21 -0500

    Draws Border
    Test to see if border could be drawn.

--- Bomberman/gamescreen.cpp
+++ Bomberman/gamescreen.cpp
@@ -31,12 +31,23 @@ void GameScreen::paintEvent(QPaintEvent *event)
     QPainter p;
     p.begin(this);
 
+    int i;
+    int j;
+
     w->drawWorld(&p);
     Character *player = w->getPlayer(w);
     player->draw(&p);
-    Blocks currentblock = w->getBlock(w,0,0);
-    
-    currentblock.draw(0,0, &p);
+    Blocks currentblock;
+
+    for(i = 0; i < 20; i++){
+        for(j = 0; j < 20; j++){
+            if(i == 0 || j == 0 || i == 19 || j == 19){
+               currentblock = w->getBlock(w,i,j);
+               currentblock.draw(i*10,j*10, &p);
+            }
+        }
+    }
+    //currentblock.draw(0,0, &p);
 
     p.end();
 }
